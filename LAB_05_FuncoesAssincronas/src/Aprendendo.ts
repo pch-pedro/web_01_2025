162b7add1e67eb9bf2e8638313760add626bb8ca
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-04-09 14:56:21 -0300

    Continuando o Lab 5 setTimeout

--- LAB_05_FuncoesAssincronas/src/Aprendendo.ts
+++ LAB_05_FuncoesAssincronas/src/Aprendendo.ts
@@ -1,3 +1,32 @@
 function concatenar (a:string, b:string): void{
-    var op = a + " "
+    var op = a + " " + b;
+    console.log("A operação efetuada resultou em: " + op);
 }
+
+function somar (a: number, b: number): void{
+    var op = a + b;
+    console.log("a operação efetuada resultou em: " +op);
+}
+
+function imprimir (value: any): void{
+    console.log("A operação efetuad resultou em: " + value);
+}
+
+function concatenar1(a: string, b: string, callback: (param: string) => void): void{
+    var op = a + " " + b;
+    callback(op);
+}
+
+function somar1 (a: number, b: number, callback: (param: number) => void): void{
+    var op = a + b;
+    callback(op);
+}
+
+concatenar1("Hello", "World", imprimir);
+
+somar1(2, 2, imprimir);
+
+//Criando uma função temporizável 
+console.log("Antes do setTimeout");
+setTimeout(() => console.log("Executando o setTimeout"), 2000);
+console.log("Depois do setTimeout");

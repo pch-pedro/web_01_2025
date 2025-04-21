1f8c2ed8e4a205230d8620d4f9a78a5bac20cab7
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-04-21 07:55:09 -0300

    Continuando LAB 4

--- LAB_04_Funcao/src/Funcao.ts
+++ LAB_04_Funcao/src/Funcao.ts
@@ -41,3 +41,87 @@ console.log(saudacao1('Pedro Lambda'));
 const multiplicar1 = (a: number, b?: number): number => b !== undefined ? a * b: a;
 
 console.log(multiplicar1(3,9));
+
+//Usando o Filter
+//Sintaxe Básica
+/*array.filter((elemento, índice, arrayOriginal) => {
+    return condição;
+})*/
+
+//Filtrando números pares
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let pares = numeros.filter(n => n%2 === 0);
+
+console.log(pares);
+
+//Filtrando números maiores que um valor
+let valores = [10, 25, 30, 45, 50];
+
+let maioresQue30 = valores.filter(n => n > 30);
+
+console.log(maioresQue30);
+
+//Filtrando palavras ocm mais de 5 letras 
+let palavras = ["sol", "chuva", "trovão", "tempestade", "vento"];
+
+let longas = palavras.filter(p => p.length > 5);
+
+console.log(longas);
+
+//Filtrando por Prioridade
+type Pessoa = {
+    nome: string;
+    idade: number;
+}
+
+const pessoas: Pessoa[] = [
+    {nome: "Ana", idade: 30},
+    {nome: "joão", idade: 20},
+    {nome: "Maria", idade: 25},
+    {nome: "Carlos", idade: 18},
+];
+
+let maioresDeIdade = pessoas.filter(pessoa => pessoa.idade >= 18);
+
+console.log(maioresDeIdade);
+
+//filtrando itens disponíveis em Estoque
+type Produto = {
+    nome: string;
+    estoque: number;
+};
+
+let produtos: Produto[] = [
+    {nome: "Camiseta", estoque: 5},
+    {nome: "Calça", estoque: 0},
+    {nome: "Tênis", estoque: 2},
+    {nome: "Bon[e", estoque: 0}
+];
+
+let disponiveis = produtos.filter(produto => produto.estoque > 0);
+
+console.log(disponiveis);
+
+//Filtrando elementos que começam com uma letra
+let nomes = ["Ana", "João", "Maria", "Carlos", "Amanda"];
+
+let comA = nomes.filter(nome => nome.startsWith("A"));
+
+console.log(comA);
+
+//Removendo valores null ou undefined
+let valores1: (number | null | undefined)[] = [10, null, 25, undefined, 40, null];
+
+let filtrados = valores.filter(v => v !== null && v !== undefined);
+
+console.log(filtrados);
+
+//Filtrando palavras que contem uma substring
+let palavras1 = ["javascript", "typescript", "java", "python", "csharp"];
+
+let comJava = palavras.filter(p => p.includes("java"));
+
+console.log(comJava);
+
+//Exercícios
